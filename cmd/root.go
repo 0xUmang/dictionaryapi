c628237fccbcd7dfbc1c97b5ed014f144776bde7
Umang <umangagarwalla111@gmail.com>
2025-05-29 00:01:30 +0530

    makefile update

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
-    "context"
-    "github.com/spf13/cobra"
+	"context"
+
+	"github.com/spf13/cobra"
 )
 
-
 var rootCmd = &cobra.Command{
-    Use:   "DictionaryApi",
-    Short: "get word definitions",	
-	Long: `A CLI tool to fetch word definitions from merriam-webster dictionary API `,
+	Use:   "dictionaryapi",
+	Short: "get word definitions",
+	Long:  `A CLI tool to fetch word definitions from merriam-webster dictionary API `,
 }
 
 func Execute(ctx context.Context) error {
-    return rootCmd.ExecuteContext(ctx)
+	return rootCmd.ExecuteContext(ctx)
 }
--- makefile
+++ makefile
@@ -1,2 +1,11 @@
-build:
-	go build -o $(GOBIN)/DictionaryApi .
+GOBIN ?=$(GOPATH)/bin
+
+include .env
+export
+
+build-maclinux:
+	export API_KEY=b48e3347-81bd-4b38-9da5-2d72399817fd
+	go build -o $(GOBIN)/dictionaryapi .
+
+build-windows:
+	go build -o dictionaryapi.exe .
